2f1296d92a9fbccf92fa623ace9b9daecc8c4a24
Nara <98963440+vianneynara@users.noreply.github.com>
2024-10-17 18:39:22 +0700

    chore: initialize project

---
+++ src/main/java/dev/realtards/wzsnacknbites/WzSnackNBitesApiApplication.java
@@ -0,0 +1,13 @@
+package dev.realtards.wzsnacknbites;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WzSnackNBitesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WzSnackNBitesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/realtards/wzsnacknbites/WzSnackNBitesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.realtards.wzsnacknbites;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WzSnackNBitesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
