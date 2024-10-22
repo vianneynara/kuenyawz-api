48d2bc3f1f829ad47b5f04555213b940ce3f90e5
narwa <narwandaru.nara@gmail.com>
2024-10-23 00:27:27 +0700

    feat: custom application configuration from properties file

---
+++ src/main/java/dev/realtards/wzsnacknbites/configurations/ApplicationProperties.java
@@ -0,0 +1,17 @@
+package dev.realtards.wzsnacknbites.configurations;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
+@Getter
+@Setter
+public class ApplicationProperties {
+
+	// Fields and default values
+	private String version = "0.0";
+	private String repositoryUrl = "https://github.com/vianneynara/wz-snack-n-bites-api";
+}
