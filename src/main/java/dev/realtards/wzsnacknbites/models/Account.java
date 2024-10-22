1f3da25f90ed975e07447c5275a771b868f0a0d2
narwa <narwandaru.nara@gmail.com>
2024-10-22 16:13:04 +0700

    feat: basic entities account, product, product variant

---
+++ src/main/java/dev/realtards/wzsnacknbites/models/Account.java
@@ -0,0 +1,50 @@
+package dev.realtards.wzsnacknbites.models;
+
+import dev.realtards.wzsnacknbites.utils.idgenerator.SnowFlakeIdValue;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@EqualsAndHashCode(callSuper = true)
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Account extends Auditable {
+
+	@Id
+	@SnowFlakeIdValue(name = "account_id")
+	private Long accountId;
+	@Column
+	private String password;
+	@Column
+	private String fullName;
+	@Column
+	private String googleId;
+	@Column
+	private String email;
+	@Column
+	private LocalDateTime email_verified_at;
+	@Column
+	private String phone;
+	@Column
+	private Privilege privilege;
+
+	/**
+	 * Type of privilege of an account.
+	 */
+	public enum Privilege {
+		ADMIN("admin"),
+		USER("user");
+
+		private final String privilege;
+
+		Privilege(String privilege) {
+			this.privilege = privilege;
+		}
+	}
+}
