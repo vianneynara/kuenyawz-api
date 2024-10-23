c94ce3e245fefa066612cb50f3bd4c519deff354
narwa <narwandaru.nara@gmail.com>
2024-10-23 20:20:47 +0700

    refactor: added setter
    Took 15 seconds

--- src/main/java/dev/realtards/wzsnacknbites/dtos/AccountRegistrationDto.java
+++ src/main/java/dev/realtards/wzsnacknbites/dtos/AccountRegistrationDto.java
@@ -3,13 +3,11 @@ package dev.realtards.wzsnacknbites.dtos;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Builder
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class AccountRegistrationDto {
