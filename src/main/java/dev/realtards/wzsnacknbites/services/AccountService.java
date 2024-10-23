529542115133e3b9cdddd705ba4ae245348bb071
narwa <narwandaru.nara@gmail.com>
2024-10-23 23:00:35 +0700

    feat: transactional annotations with new service methods
    Took 27 seconds

--- src/main/java/dev/realtards/wzsnacknbites/services/AccountService.java
+++ src/main/java/dev/realtards/wzsnacknbites/services/AccountService.java
@@ -1,21 +1,37 @@
 package dev.realtards.wzsnacknbites.services;
 
+import dev.realtards.wzsnacknbites.dtos.AccountPatchDto;
 import dev.realtards.wzsnacknbites.dtos.AccountRegistrationDto;
+import dev.realtards.wzsnacknbites.dtos.AccountPutDto;
+import dev.realtards.wzsnacknbites.dtos.PasswordUpdateDto;
 import dev.realtards.wzsnacknbites.models.Account;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface AccountService {
 
+	@Transactional(readOnly = true)
 	List<Account> getAllAccounts();
 
+	@Transactional
 	Account createAccount(AccountRegistrationDto accountRegistrationDto);
 
+	@Transactional(readOnly = true)
 	Account getAccount(long accountId);
 
-	Account updateAccount(Account account);
+	@Transactional
+	Account updateAccount(Long accountId, AccountPutDto accountPutDto);
 
+	@Transactional
 	void deleteAccount(long accountId);
 
+	@Transactional
+	Account patchAccount(Long accountId, AccountPatchDto accountPatchDto);
+
+	@Transactional
+	Account updatePassword(Long accountId, PasswordUpdateDto passwordUpdateDto);
+
+	@Transactional
 	Account updatePrivilege(Account account, Account.Privilege privilege);
 }
