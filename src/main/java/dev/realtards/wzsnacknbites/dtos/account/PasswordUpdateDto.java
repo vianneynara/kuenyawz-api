590a19bf0f2d25980d8039b2c12b9fa2b7962703
narwa <narwandaru.nara@gmail.com>
2024-10-24 12:30:56 +0700

    refactor: privilege update dto
    Took 1 minute

--- src/main/java/dev/realtards/wzsnacknbites/dtos/account/PasswordUpdateDto.java
+++ src/main/java/dev/realtards/wzsnacknbites/dtos/account/PasswordUpdateDto.java
@@ -3,10 +3,8 @@ package dev.realtards.wzsnacknbites.dtos.account;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
 @AllArgsConstructor
 public class PasswordUpdateDto {
 	@NotBlank(message = "Current password is required")
