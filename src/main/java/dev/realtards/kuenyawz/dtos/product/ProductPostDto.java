83460157a874d54da3e24d7fd032848dd8437c1a
narwa <narwandaru.nara@gmail.com>
2024-10-26 21:19:15 +0700

    chore: optimize imports
    Took 2 minutes

--- src/main/java/dev/realtards/kuenyawz/dtos/product/ProductPostDto.java
+++ src/main/java/dev/realtards/kuenyawz/dtos/product/ProductPostDto.java
@@ -1,6 +1,5 @@
 package dev.realtards.kuenyawz.dtos.product;
 
-import dev.realtards.kuenyawz.entities.Product;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
