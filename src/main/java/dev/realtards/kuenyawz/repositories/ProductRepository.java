f9e244b232698990f2de5480cc4dd073b481f64f
narwa <narwandaru.nara@gmail.com>
2024-10-27 20:16:21 +0700

    feat: product repository
    Not checked as there's no custom queries or optimization needs
    
    Took 2 minutes

---
+++ src/main/java/dev/realtards/kuenyawz/repositories/ProductRepository.java
@@ -0,0 +1,13 @@
+package dev.realtards.kuenyawz.repositories;
+
+import dev.realtards.kuenyawz.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+	List<Product> findAllByNameLikeIgnoreCase(String name);
+
+	List<Product> findAllByCategoryIs(Product.Category category);
+}
