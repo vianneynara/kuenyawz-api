5e537200e29ebba58da4e6d8c4684f434faee0ed
narwa <narwandaru.nara@gmail.com>
2024-10-30 00:16:20 +0700

    feat: repository for product image with custom query
    Took 45 seconds

---
+++ src/main/java/dev/realtards/kuenyawz/repositories/ProductImageRepository.java
@@ -0,0 +1,21 @@
+package dev.realtards.kuenyawz.repositories;
+
+import dev.realtards.kuenyawz.entities.ProductImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductImageRepository extends JpaRepository<ProductImage, Long> {
+
+	List<ProductImage> findByProduct_ProductId(Long productId);
+
+	Optional<ProductImage> findByProduct_ProductIdAndProductImageId(Long productId, Long productImageId);
+
+	@Modifying
+	@Query("DELETE FROM ProductImage pi WHERE pi.product.productId = :productId")
+	int deleteAllByProduct_ProductId(@Param("productId") Long productId);
+}
