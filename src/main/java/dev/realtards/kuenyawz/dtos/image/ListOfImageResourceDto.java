61ae4686976fe6d487b74a0662b09a0831ce5197
narwa <narwandaru.nara@gmail.com>
2024-10-31 06:55:33 +0700

    feat: response with list of resource DTO
    Took 1 hour 2 minutes

---
+++ src/main/java/dev/realtards/kuenyawz/dtos/image/ListOfImageResourceDto.java
@@ -0,0 +1,13 @@
+package dev.realtards.kuenyawz.dtos.image;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import java.util.List;
+
+@Schema(description = "Response object for list of resource information DTOs")
+public record ListOfImageResourceDto(
+
+	@Schema(description = "List of image resource information DTOs")
+	List<ImageResourceDTO> images
+) {
+}
