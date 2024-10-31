bb16ca7ffd7380b903930a9f5e1b23958873e9a7
narwa <narwandaru.nara@gmail.com>
2024-11-01 00:49:13 +0700

    docs: added tag name for simulator controller
    Took 42 seconds

--- src/main/java/dev/realtards/kuenyawz/controllers/SimulatorController.java
+++ src/main/java/dev/realtards/kuenyawz/controllers/SimulatorController.java
@@ -1,10 +1,12 @@
 package dev.realtards.kuenyawz.controllers;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Tag(name = "Simulator", description = "Simulator pages")
 @Controller
 @RequestMapping("/sim")
 public class SimulatorController extends BaseController {
