2b701bd780fccfd6f85dba1004bf4f03fb064040
narwa <narwandaru.nara@gmail.com>
2024-11-04 20:28:48 +0700

    feat: service interface for importing products from CSV
    Took 48 minutes

---
+++ src/main/java/dev/realtards/kuenyawz/services/ProductCsvImportService.java
@@ -0,0 +1,25 @@
+package dev.realtards.kuenyawz.services;
+
+import dev.realtards.kuenyawz.dtos.product.ProductCsvPostDto;
+import dev.realtards.kuenyawz.dtos.product.ProductPostDto;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+public interface ProductCsvImportService {
+	/**
+	 * Executes the process of parsing from a CSV file and saves it to the database.
+	 * @param file {@link MultipartFile} CSV file to be imported
+	 * @throws IOException
+	 */
+	void importProductsFromCsv(MultipartFile file);
+
+	void importProductsFromDto(ProductCsvPostDto productCsvPostDto);
+
+	/**
+	 * Parses a line of string, assumed to be data row of Product CSV file to ProductPostDto.
+	 * @param line {@link String} data row of the CSV file
+	 * @return {@link ProductPostDto} parsed data to ProductPostDto
+	 */
+	ProductPostDto parseLineToProductDto(String line);
+}
