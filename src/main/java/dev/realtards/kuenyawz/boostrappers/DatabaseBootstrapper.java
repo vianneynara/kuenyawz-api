d325158eaec2e07686ff3f3ff69a4e5cb3d04d4b
narwa <narwandaru.nara@gmail.com>
2024-11-04 22:25:41 +0700

    feat: grab the resource and insert the product seeder
    Took 32 seconds

--- src/main/java/dev/realtards/kuenyawz/boostrappers/DatabaseBootstrapper.java
+++ src/main/java/dev/realtards/kuenyawz/boostrappers/DatabaseBootstrapper.java
@@ -8,12 +8,19 @@ import dev.realtards.kuenyawz.dtos.product.VariantPostDto;
 import dev.realtards.kuenyawz.entities.Account;
 import dev.realtards.kuenyawz.exceptions.AccountExistsException;
 import dev.realtards.kuenyawz.services.AccountService;
+import dev.realtards.kuenyawz.services.ProductCsvImportService;
 import dev.realtards.kuenyawz.services.ProductService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.ListIterator;
@@ -25,6 +32,9 @@ public class DatabaseBootstrapper implements CommandLineRunner {
 
 	private final AccountService accountService;
 	private final ProductService productService;
+	private final ProductCsvImportService productCsvImportService;
+
+	private final String PATH_TO_PRODUCT_SEEDER = "seeders/ProductsSeeder.csv";
 
 	private final List<AccountRegistrationDto> BOOTSTRAP_ACCOUNTS = List.of(
 		AccountRegistrationDto.builder()
@@ -173,11 +183,30 @@ public class DatabaseBootstrapper implements CommandLineRunner {
 		}
 	}
 
+	private void injectProductsFromCSV() {
+		try {
+			ClassPathResource resource = new ClassPathResource(PATH_TO_PRODUCT_SEEDER);
+			File file = resource.getFile();
+
+			try (FileInputStream fis = new FileInputStream(file)) {
+				MultipartFile multipartFile = new MockMultipartFile(
+					"file",
+					file.getName(),
+					"text/csv",
+					fis
+				);
+				productCsvImportService.importProductsFromCsv(multipartFile);
+			}
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
 	@Override
 	public void run(String... args) {
 		log.info("Bootstrapping database...");
 		injectAccounts();
-		injectProducts();
+		injectProductsFromCSV();
 
 		log.info("Database bootstrapping complete");
 	}
