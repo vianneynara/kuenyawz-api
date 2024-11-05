6bbcbd894309cf6bda2565bfdc575e22d6b12596
narwa <narwandaru.nara@gmail.com>
2024-11-05 20:50:09 +0700

    feat: unused method to document manual query with versioning
    Took 1 minute

--- src/main/java/dev/realtards/kuenyawz/repositories/ProductRepository.java
+++ src/main/java/dev/realtards/kuenyawz/repositories/ProductRepository.java
@@ -2,6 +2,9 @@ package dev.realtards.kuenyawz.repositories;
 
 import dev.realtards.kuenyawz.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -12,6 +15,16 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 	List<Product> findAllByCategoryIs(Product.Category category);
 
     boolean existsByNameIgnoreCase(String name);
+
+    /**
+     * This is not used, but is good to document for future reference.
+     */
+    @Modifying
+    @Query("UPDATE Product p SET " +
+        "p.deleted = true, " +
+        "p.version = (p.version + 1) " +
+        "WHERE p.productId = :productId AND p.version = :version")
+    int softDeleteById(@Param("productId") Long id, @Param("version") Long version);
 
     List<Product> findAllByDeletedTrue();
 }
