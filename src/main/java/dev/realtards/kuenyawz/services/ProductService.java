f825d60f50330383589d3b4d1438608db09841b8
narwa <narwandaru.nara@gmail.com>
2024-11-12 12:27:48 +0700

    feat: added pageable method to get all products
    Took 14 seconds

--- src/main/java/dev/realtards/kuenyawz/services/ProductService.java
+++ src/main/java/dev/realtards/kuenyawz/services/ProductService.java
@@ -4,6 +4,7 @@ import dev.realtards.kuenyawz.dtos.product.ProductDto;
 import dev.realtards.kuenyawz.dtos.product.ProductPatchDto;
 import dev.realtards.kuenyawz.dtos.product.ProductPostDto;
 import dev.realtards.kuenyawz.exceptions.ResourceNotFoundException;
+import org.springframework.data.domain.Page;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -15,12 +16,22 @@ public interface ProductService {
 	 *
 	 * @param category {@link String} optional category filter
 	 * @param keyword  {@link String} optional keyword filter
-	 * @param page {@link Integer} optional page number
-	 * @param pageSize {@link Integer} optional page size
 	 * @return {@link List} of {@link ProductDto}
 	 */
 	@Transactional(readOnly = true)
-	List<ProductDto> getAllProducts(String category, String keyword, Integer page, Integer pageSize);
+	List<ProductDto> getAllProducts(String category, String keyword);
+
+	/**
+	 * Master method to get all products with paginated result.
+	 *
+	 * @param category {@link String} optional category filter
+	 * @param keyword  {@link String} optional keyword filter
+	 * @param page     {@link Integer} optional page number
+	 * @param pageSize {@link Integer} optional page size
+	 * @return {@link Page} of {@link ProductDto}
+	 */
+	@Transactional(readOnly = true)
+	Page<ProductDto> getAllProductsPaginated(String category, String keyword, Integer page, Integer pageSize);
 
 	/**
 	 * Creates a new product from the DTO with the provided variants' DTOs.
