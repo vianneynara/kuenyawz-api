486b55cce5a24466c4df730073938b7e223e1a6b
narwa <narwandaru.nara@gmail.com>
2024-11-15 21:25:13 +0700

    refactor: request matchers structure
    Took 26 seconds

--- src/main/java/dev/realtards/kuenyawz/configurations/security/SecurityConfig.java
+++ src/main/java/dev/realtards/kuenyawz/configurations/security/SecurityConfig.java
@@ -1,68 +1,54 @@
 package dev.realtards.kuenyawz.configurations.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+	private final AuthenticationProvider authenticationProvider;
+	private final JwtAuthorizationFilter jwtAuthorizationFilter;
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity httpSec) throws Exception {
 		httpSec
 			.csrf(csrf -> csrf.ignoringRequestMatchers("/h2-console/**", "/api/**"))
 			.authorizeHttpRequests(auth -> auth
-					.requestMatchers(
-						"api/accounts/**").permitAll()
-//					.requestMatchers(HttpMethod.PATCH,
-//						"/api/accounts/**").hasAnyRole("ADMIN", "USER")
-					.requestMatchers(
-						"/api/sim/**").permitAll()
-					.requestMatchers(HttpMethod.GET,
-						"/api/products",
-						"/api/products/**").permitAll()
-					.requestMatchers(
-						"/api/products",
-						"/api/products/**").hasRole("ADMIN")
-					.requestMatchers(HttpMethod.GET,
-						"/api/images").permitAll()
-					.requestMatchers(
-						"/api/images").hasRole("ADMIN")
-					.requestMatchers(HttpMethod.GET,
-						"/api",
-						"/api/status").permitAll()
-					.requestMatchers(HttpMethod.GET,
-						"/api/images/**").permitAll()
-//					.anyRequest().permitAll()
+				.requestMatchers("/api/accounts/**").permitAll()
+				.requestMatchers("/api/sim/**").permitAll()
+				.requestMatchers(HttpMethod.GET,
+					"/api/products",
+					"/api/products/**",
+					"/api/images/**",
+					"/api",
+					"/api/status").permitAll()
+				.requestMatchers(HttpMethod.PATCH, "/api/accounts/**").hasAnyRole("ADMIN", "USER")
+				.requestMatchers("/api/products", "/api/products/**", "/api/images").hasRole("ADMIN")
+				.anyRequest().authenticated()
 			)
-			.headers(headers -> headers
-				.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)
-			)
-			.sessionManagement(session -> session
-				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-			)
-			.httpBasic(Customizer.withDefaults())
+			.headers(hs -> hs.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+			.sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+			.authenticationProvider(authenticationProvider)
+			.addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class)
 
 			// Special handler for 401 and 403
 			.exceptionHandling(exc -> exc
@@ -91,23 +77,5 @@ public class SecurityConfig {
 				"/swagger-ui/**",
 				"/swagger-ui.html"
 			);
-	}
-
-	@Bean
-	public AuthenticationProvider authenticationProvider(AccountUserDetailsService userDetailsService) {
-		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-		provider.setUserDetailsService(userDetailsService);
-		provider.setPasswordEncoder(passwordEncoder());
-		return provider;
-	}
-
-	@Bean
-	public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
-		return authConfig.getAuthenticationManager();
-	}
-
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
 	}
 }
