2385d0b921a78885280b5910d95b5067f92f5e56
narwa <narwandaru.nara@gmail.com>
2024-11-16 16:38:37 +0700

    ondev: comment this shit
    Took 21 seconds

--- src/main/java/dev/realtards/kuenyawz/controllers/AuthController.java
+++ src/main/java/dev/realtards/kuenyawz/controllers/AuthController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
 
-	private final AuthService authService;
+//	private final AuthService authService;
 
 	@Operation(summary = "Register a new user")
 	@ApiResponses({
