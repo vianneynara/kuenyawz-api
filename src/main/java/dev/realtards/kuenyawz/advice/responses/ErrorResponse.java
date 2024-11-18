8afead31c5dcbddcac330d85feba7d8480ae7cdf
narwa <narwandaru.nara@gmail.com>
2024-11-18 22:52:35 +0700

    refactor: remove lombok @Getter for record
    Took 3 minutes

--- src/main/java/dev/realtards/kuenyawz/advice/responses/ErrorResponse.java
+++ src/main/java/dev/realtards/kuenyawz/advice/responses/ErrorResponse.java
@@ -1,10 +1,8 @@
 package dev.realtards.kuenyawz.advice.responses;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.Getter;
 
 @Schema(description = "Response containing error message")
-@Getter
 public record ErrorResponse(
 	@Schema(description = "Error message") String message
 ) {
--- src/main/java/dev/realtards/kuenyawz/dtos/account/ListOfAccountDto.java
+++ src/main/java/dev/realtards/kuenyawz/dtos/account/ListOfAccountDto.java
@@ -1,12 +1,10 @@
 package dev.realtards.kuenyawz.dtos.account;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.Getter;
 
 import java.util.List;
 
 @Schema(description = "Response containing list of accounts")
-@Getter
 public record ListOfAccountDto(
 	@Schema(description = "List of account secure information") List<AccountSecureDto> accounts) {
 	public ListOfAccountDto(List<AccountSecureDto> accounts) {
