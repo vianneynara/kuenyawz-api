3bbdcfddef006f94a1f1f26494fe3f07e5ad30f2
narwa <narwandaru.nara@gmail.com>
2024-11-18 22:53:44 +0700

    refactor: remove lombok @Getter for record
    Took 48 seconds

--- src/main/java/dev/realtards/kuenyawz/advice/responses/ListedErrors.java
+++ src/main/java/dev/realtards/kuenyawz/advice/responses/ListedErrors.java
@@ -1,12 +1,10 @@
 package dev.realtards.kuenyawz.advice.responses;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.Getter;
 
 import java.util.Map;
 
 @Schema(description = "Response containing error message and list of errors")
-@Getter
 public record ListedErrors<T extends Map<String, String>>(
 	@Schema(description = "Error message") String message,
 	@Schema(description = "Map of errors, mapped by field name") T errors
