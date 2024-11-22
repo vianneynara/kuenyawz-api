d648745f4b23e8982f1c0d3309676174217a7e42
narwa <narwandaru.nara@gmail.com>
2024-11-22 23:45:18 +0700

    docs(CartItemMapper): added full CartItemDto mapping support
    Took 1 minute

--- src/main/java/dev/realtards/kuenyawz/mapper/CartItemMapper.java
+++ src/main/java/dev/realtards/kuenyawz/mapper/CartItemMapper.java
@@ -1,17 +1,19 @@
 package dev.realtards.kuenyawz.mapper;
 
 import dev.realtards.kuenyawz.dtos.cartItem.CartItemDto;
-import dev.realtards.kuenyawz.dtos.cartItem.CartItemPatchDto;
-import dev.realtards.kuenyawz.dtos.cartItem.CartItemPostDto;
 import dev.realtards.kuenyawz.dtos.product.ProductDto;
 import dev.realtards.kuenyawz.entities.CartItem;
-import org.mapstruct.*;
+import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface CartItemMapper {
-    CartItem toEntity(CartItemPostDto cartItemPostDto);
 
-    CartItemDto fromEntity(CartItem cartItem, ProductDto productDto);
+	CartItemDto fromEntity(CartItem cartItem);
 
-    CartItem updateCartItemFromPatch(CartItem cartItem, CartItemPatchDto cartItemPatchDto);
+	default CartItemDto fromEntity(CartItem cartItem, ProductDto productDto, Long selectedVariantId) {
+		CartItemDto dto = fromEntity(cartItem);
+		dto.setProduct(productDto);
+		dto.setSelectedVariantId(selectedVariantId);
+		return dto;
+	}
 }
