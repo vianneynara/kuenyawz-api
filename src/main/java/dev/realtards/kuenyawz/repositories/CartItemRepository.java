2bde826020b3dc28ff971d5af410245120a2d118
narwa <narwandaru.nara@gmail.com>
2024-11-22 22:03:03 +0700

    fix(CartItemRepository): fix JPA conventional query naming and consistency
    Took 29 seconds

--- src/main/java/dev/realtards/kuenyawz/repositories/CartItemRepository.java
+++ src/main/java/dev/realtards/kuenyawz/repositories/CartItemRepository.java
@@ -2,22 +2,18 @@ package dev.realtards.kuenyawz.repositories;
 
 import dev.realtards.kuenyawz.entities.CartItem;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
-    List<CartItem> findAllByAccountId(long accountId);
 
-    Page<CartItem> findAll(Long accountId, PageRequest pageRequest);
+    List<CartItem> findAllByAccount_AccountId(Long accountId);
 
-    int countCartItemByAccountId(long accountId);
+    Page<CartItem> findAll(Long accountId, Pageable pageRequest);
 
-    @Modifying
-    @Query("DELETE FROM CartItem c WHERE c.account.accountId = :accountId")
-    void deleteByAccount_AccountId(@Param("accountId") Long accountId);
+    int countCartItemByAccount_AccountId(Long accountId);
+
+    void deleteByAccount_AccountId(Long accountId);
 }
