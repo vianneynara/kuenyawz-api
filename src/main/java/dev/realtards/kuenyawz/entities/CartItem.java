91aa133ddf1f9d6879db23cf0daecbcc8204cdad
narwa <narwandaru.nara@gmail.com>
2024-11-22 22:56:03 +0700

    feat(CartItem): added patch from DTO method
    Took 51 minutes

--- src/main/java/dev/realtards/kuenyawz/entities/CartItem.java
+++ src/main/java/dev/realtards/kuenyawz/entities/CartItem.java
@@ -1,5 +1,6 @@
 package dev.realtards.kuenyawz.entities;
 
+import dev.realtards.kuenyawz.dtos.cartItem.CartItemPatchDto;
 import dev.realtards.kuenyawz.utils.idgenerator.SnowFlakeIdValue;
 import jakarta.persistence.*;
 import lombok.*;
@@ -29,4 +30,14 @@ public class CartItem {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "account_id", nullable = false)
     private Account account;
+
+    public CartItem patchFromDto(CartItemPatchDto cartItemPatchDto) {
+        if (cartItemPatchDto.getQuantity() != null)
+            this.quantity = cartItemPatchDto.getQuantity();
+
+        if (cartItemPatchDto.getNote() != null)
+            this.note = cartItemPatchDto.getNote();
+
+        return this;
+    }
 }
