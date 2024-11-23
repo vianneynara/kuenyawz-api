b46551b27b81485e01c95a8ec96f16b8d184f7d7
narwa <narwandaru.nara@gmail.com>
2024-11-23 17:18:55 +0700

    feat(CustomSchedule): entity and enum for schedule type
    Took 5 minutes

---
+++ src/main/java/dev/realtards/kuenyawz/entities/CustomSchedule.java
@@ -0,0 +1,53 @@
+package dev.realtards.kuenyawz.entities;
+
+import dev.realtards.kuenyawz.utils.idgenerator.SnowFlakeIdValue;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.util.Date;
+
+@Entity
+@Data
+@EqualsAndHashCode(callSuper = true)
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomSchedule extends Auditables {
+
+	@Id
+	@SnowFlakeIdValue(name = "custom_schedule_id")
+	@Column(name = "custom_schedule_id", columnDefinition = "BIGINT", updatable = false, nullable = false)
+	private Long customScheduleId;
+
+	@Column(nullable = false)
+	private ScheduleType scheduleType = ScheduleType.CLOSED;
+
+	@Column
+	private String scheduleName;
+
+	@Column
+	private Date date;
+
+	public enum ScheduleType {
+		CLOSED("CLOSED"),
+		ONE_TIME("ONE_TIME"),
+		RECURRING("RECURRING");
+
+		String value;
+
+		ScheduleType(String name) {
+			this.value = name;
+		}
+
+		ScheduleType fromString(String name) {
+			for (ScheduleType type : ScheduleType.values()) {
+				if (type.value.equals(name)) {
+					return type;
+				}
+			}
+			return null;
+		}
+	}
+}
