425aa526933cf04ae6f658a7c5296fa9c85598ff
ZeroFairy <jordanjv453@gmail.com>
2024-11-26 01:17:22 +0700

    Update the return type of the method findFrequentSetItemWith and Added method findAllFrequentSetOfItems

--- src/main/java/dev/kons/kuenyawz/services/logic/AprioriService.java
+++ src/main/java/dev/kons/kuenyawz/services/logic/AprioriService.java
@@ -6,13 +6,21 @@ import java.util.Set;
 public interface AprioriService {
 
     /**
+     * Find frequent of each item(Product)
+     *
+     * @return Set<Map < Long ( ProductId ), Set < Long ( ProductId )>>>
+     * @input Set<Map < Long ( OrderId ), Set < Long ( ProductId )>>>
+     */
+    Set<Map<Long, Set<Long>>> findAllFrequentSetOfItems(Set<Map<Long, Set<Long>>> orders);
+
+    /**
      * Find frequent of one item(Product), to find the match frequent item with the selected item
      *
      * @param orders Set<Map < Long ( OrderId ), Set < Long ( ProductId )>>>
      * @param productId The selected product
      * @return
      */
-    Set<Map<Long, Set<Long>>> findFrequentSetItemWith(Set<Map<Long, Set<Long>>> orders, Long productId);
+    Map<Long, Set<Long>> findFrequentSetItemWith(Set<Map<Long, Set<Long>>> orders, Long productId);
 
     /**
      * Find frequent of one item(Product), to find the match frequent item with the selected item
@@ -22,7 +30,7 @@ public interface AprioriService {
      * @param topN Number of frequent item that going to be show
      * @return
      */
-    Set<Map<Long, Set<Long>>> findFrequentSetItemWith(Set<Map<Long, Set<Long>>> orders, Long productId, int topN);
+    Map<Long, Set<Long>> findFrequentSetItemWith(Set<Map<Long, Set<Long>>> orders, Long productId, int topN);
 
     /**
      * Find the most buy frequent item
