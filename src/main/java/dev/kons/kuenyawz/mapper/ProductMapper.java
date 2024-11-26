22deb777e4a6bba1d9868db1625bfcef25444bdc
narwa <narwandaru.nara@gmail.com>
2024-11-26 14:12:42 +0700

    refactor(ProductMapper): experimental custom image setter in Mapstruct mapper
    Took 39 seconds

--- src/main/java/dev/kons/kuenyawz/mapper/ProductMapper.java
+++ src/main/java/dev/kons/kuenyawz/mapper/ProductMapper.java
@@ -3,11 +3,13 @@ package dev.kons.kuenyawz.mapper;
 import dev.kons.kuenyawz.dtos.product.ProductDto;
 import dev.kons.kuenyawz.dtos.product.ProductPatchDto;
 import dev.kons.kuenyawz.dtos.product.ProductPostDto;
+import dev.kons.kuenyawz.dtos.product.VariantDto;
 import dev.kons.kuenyawz.entities.Product;
-import org.mapstruct.BeanMapping;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
+import dev.kons.kuenyawz.services.logic.ImageStorageService;
+import org.mapstruct.*;
+
+import java.util.ArrayList;
+import java.util.Comparator;
 
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
@@ -22,4 +24,24 @@ public interface ProductMapper {
     @Mapping(target = "images", ignore = true)
 	@BeanMapping(nullValuePropertyMappingStrategy = org.mapstruct.NullValuePropertyMappingStrategy.IGNORE)
 	Product updateProductFromPatch(ProductPatchDto dto, @MappingTarget Product product);
+
+	/**
+	 * This is used to fill the images after mapping the entity to dto.
+	 *
+	 * @param product Source
+	 * @param productDto Target
+	 * @param imageStorageService Image storage service that will be injected by MapStruct
+	 */
+    @AfterMapping
+    default void enrichProductDto(
+        Product product,
+        @MappingTarget ProductDto productDto,
+        @Context ImageStorageService imageStorageService
+    ) {
+        if (productDto.getVariants() == null) {
+           productDto.setVariants(new ArrayList<>());
+        }
+        productDto.getVariants().sort(Comparator.comparing(VariantDto::getVariantId));
+        productDto.setImages(imageStorageService.getImageUrls(product));
+    }
 }
