40d0fcd6faf853678e9199c019d35ebc206bc082
narwa <narwandaru.nara@gmail.com>
2024-11-27 03:44:05 +0700

    feat(MidtransApiService): fetch transaction status method
    Took 8 minutes

--- src/main/java/dev/kons/kuenyawz/services/logic/MidtransApiService.java
+++ src/main/java/dev/kons/kuenyawz/services/logic/MidtransApiService.java
@@ -6,6 +6,8 @@ import dev.kons.kuenyawz.dtos.midtrans.TransactionResponse;
 public interface MidtransApiService {
 	/**
 	 * Creates a transaction in which will return a token and a redirect URL.
+	 * <br>
+	 * {@code POST https://app.sandbox.midtrans.com/snap/v1/transactions}
 	 *
 	 * @param request {@link TransactionRequest} transaction request
 	 * @return {@link TransactionResponse} transaction response
@@ -13,7 +15,19 @@ public interface MidtransApiService {
 	TransactionResponse createTransaction(TransactionRequest request);
 
 	/**
+	 * Fetches a transaction status by its order (purchase) id.
+	 * <br>
+	 * {@code GET https://api.sandbox.midtrans.com/v2/{order_id}/status}
+	 *
+	 * @param orderId {@link String} order id
+	 * @return {@link TransactionResponse} transaction response
+	 */
+	TransactionResponse fetchTransactionStatus(String orderId);
+
+	/**
 	 * Cancels/voids a transaction by its order (purchase) id.
+	 * <br>
+	 * {@code POST https://api.sandbox.midtrans.com/v2/{order_id}/cancel}
 	 *
 	 * @param orderId {@link String} order id
 	 * @return {@link TransactionResponse} transaction response
