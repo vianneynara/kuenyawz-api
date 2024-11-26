52d4e934252f2467cfbf261e3a7c1a81c442b943
narwa <narwandaru.nara@gmail.com>
2024-11-26 13:26:08 +0700

    refactor(PurchaseMapper): mapper dependencies and coordinate to latitude longitude
    Took 2 minutes

--- src/main/java/dev/kons/kuenyawz/mapper/PurchaseMapper.java
+++ src/main/java/dev/kons/kuenyawz/mapper/PurchaseMapper.java
@@ -3,9 +3,12 @@ package dev.kons.kuenyawz.mapper;
 import dev.kons.kuenyawz.dtos.purchase.PurchaseDto;
 import dev.kons.kuenyawz.entities.Purchase;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {PurchaseItemMapper.class, TransactionMapper.class})
 public interface PurchaseMapper {
 
+    @Mapping(target = "latitude", source = "coordinate.latitude")
+    @Mapping(target = "longitude", source = "coordinate.longitude")
 	PurchaseDto toDto(Purchase entity);
 }
