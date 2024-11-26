c43e88b8e1b0342ec886338d44201fae52148e69
narwa <narwandaru.nara@gmail.com>
2024-11-26 15:25:22 +0700

    refactor(PurchaseMapper): updateFromPatchDto with required dependencies
    Took 42 seconds
    
    Took 5 minutes

--- src/main/java/dev/kons/kuenyawz/mapper/PurchaseMapper.java
+++ src/main/java/dev/kons/kuenyawz/mapper/PurchaseMapper.java
@@ -1,9 +1,9 @@
 package dev.kons.kuenyawz.mapper;
 
 import dev.kons.kuenyawz.dtos.purchase.PurchaseDto;
+import dev.kons.kuenyawz.dtos.purchase.PurchasePatchDto;
 import dev.kons.kuenyawz.entities.Purchase;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
+import org.mapstruct.*;
 
 @Mapper(componentModel = "spring", uses = {PurchaseItemMapper.class, TransactionMapper.class})
 public interface PurchaseMapper {
@@ -11,4 +11,10 @@ public interface PurchaseMapper {
     @Mapping(target = "latitude", source = "coordinate.latitude")
     @Mapping(target = "longitude", source = "coordinate.longitude")
 	PurchaseDto toDto(Purchase entity);
+
+    @Mapping(target = "version", ignore = true)
+    @Mapping(target = "coordinate.latitude", source = "latitude")
+    @Mapping(target = "coordinate.longitude", source = "longitude")
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+	Purchase updateFromPatchDto(PurchasePatchDto dto, @MappingTarget Purchase entity);
 }
