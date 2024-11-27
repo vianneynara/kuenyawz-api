898c18857ecdbefb4d63a56989033eb864874582
narwa <narwandaru.nara@gmail.com>
2024-11-27 21:02:24 +0700

    feat(PurchaseDto): added delivery option and fee
    Took 18 seconds

--- src/main/java/dev/kons/kuenyawz/dtos/purchase/PurchaseDto.java
+++ src/main/java/dev/kons/kuenyawz/dtos/purchase/PurchaseDto.java
@@ -1,6 +1,7 @@
 package dev.kons.kuenyawz.dtos.purchase;
 
 import dev.kons.kuenyawz.constants.PaymentType;
+import dev.kons.kuenyawz.entities.Coordinate;
 import dev.kons.kuenyawz.entities.Purchase;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
@@ -23,26 +24,26 @@ public class PurchaseDto {
 	@Schema(description = "Purchase id", example = "12345678")
 	private Long purchaseId;
 
-	@Schema(description = "Purchase date", example = "2024-04-21")
-	private LocalDate purchaseDate;
-
 	@Schema(description = "Full address of the purchase", example = "Merdeka Square, Jakarta, Jalan Lapangan Monas, Gambir, Central Jakarta City, Jakarta 10110")
 	private String fullAddress;
 
-	@Schema(description = "Longitude for the purchase place", example = "-6.1745517")
-	private Double latitude;
+	@Schema(description = "Coordinate of the event")
+	private Coordinate coordinate;
 
-	@Schema(description = "Longitude for the purchase place", example = "106.8205983")
-	private Double longitude;
-
-	@Schema(description = "Purchase status", example = "PENDING")
-	private Purchase.PurchaseStatus status;
+	@Schema(description = "Event date", example = "2024-04-21")
+	private LocalDate eventDate;
 
 	@Schema(description = "Payment type of the purchase", example = "DOWN_PAYMENT")
 	private PaymentType paymentType;
 
-	@Schema(description = "Fee of the purchase", example = "10000.00")
-	private BigDecimal fee;
+	@Schema(description = "Delivery option of the purchase", example = "DELIVERY")
+	private Purchase.DeliveryOption deliveryOption;
+
+	@Schema(description = "Delivery fee of the purchase", example = "10000.00")
+	private BigDecimal deliveryFee;
+
+	@Schema(description = "Purchase status", example = "PENDING")
+	private Purchase.PurchaseStatus status;
 
 	@Schema(description = "Items of the purchase")
 	private List<PurchaseItemDto> purchaseItems;
