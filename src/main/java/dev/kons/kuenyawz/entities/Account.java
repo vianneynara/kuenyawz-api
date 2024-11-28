4a7fe9d070caf8bbadfc963307357601a9787adb
narwa <narwandaru.nara@gmail.com>
2024-11-28 16:27:33 +0700

    refactor(getEmail): methods to filter email and lastname
    Took 5 hours 44 minutes

--- src/main/java/dev/kons/kuenyawz/entities/Account.java
+++ src/main/java/dev/kons/kuenyawz/entities/Account.java
@@ -61,12 +61,17 @@ public class Account extends Auditables implements UserDetails {
 	@OneToMany(mappedBy = "account", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
 	private Set<Transaction> transactions;
 
+	public String getEmail() {
+		return email != null ? email : "";
+	}
+
 	public String getFirstName() {
 		return fullName.split(" ")[0];
 	}
 
 	public String getLastName() {
-		return fullName.split(" ", 1)[1];
+		var splitted = fullName.split(" ", 1);
+		return splitted.length > 1 ? splitted[1] : "";
 	}
 
 	@Override
