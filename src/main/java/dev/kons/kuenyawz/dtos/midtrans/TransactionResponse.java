ab777bd934d74ec0c4a7330230cee72895fe795c
narwa <narwandaru.nara@gmail.com>
2024-11-29 14:15:11 +0700

    feat(TransactionResponse): added builder for idk

--- src/main/java/dev/kons/kuenyawz/dtos/midtrans/TransactionResponse.java
+++ src/main/java/dev/kons/kuenyawz/dtos/midtrans/TransactionResponse.java
@@ -2,6 +2,7 @@ package dev.kons.kuenyawz.dtos.midtrans;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -11,6 +12,7 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class TransactionResponse {
     private String token;
 
