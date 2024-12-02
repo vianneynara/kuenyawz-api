ca20bc60a91d7dac62dbeb722c9b010ab87c7673
narwa <narwandaru.nara@gmail.com>
2024-12-02 14:20:13 +0700

    refactor(TransactionController): enable fetch transaction status
    Took 33 seconds

--- src/main/java/dev/kons/kuenyawz/controllers/TransactionController.java
+++ src/main/java/dev/kons/kuenyawz/controllers/TransactionController.java
@@ -4,16 +4,17 @@ import dev.kons.kuenyawz.dtos.purchase.TransactionDto;
 import dev.kons.kuenyawz.entities.Account;
 import dev.kons.kuenyawz.services.entity.TransactionService;
 import dev.kons.kuenyawz.services.logic.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 
@@ -55,13 +56,13 @@ public class TransactionController {
 		return ResponseEntity.ok(result);
 	}
 
-//	@Operation(summary = "Fetch transaction status")
-//	@ApiResponses({
-//		@ApiResponse(responseCode = "200", description = "Transaction status fetched successfully"),
-//		@ApiResponse(responseCode = "400", description = "Bad request")
-//	})
-//	@SecurityRequirement(name = "cookieAuth")
-//	@GetMapping("/{transactionId}")
+	@Operation(summary = "Fetch transaction status")
+	@ApiResponses({
+		@ApiResponse(responseCode = "200", description = "Transaction status fetched successfully"),
+		@ApiResponse(responseCode = "400", description = "Bad request")
+	})
+	@SecurityRequirement(name = "cookieAuth")
+	@GetMapping("/{transactionId}")
 	public ResponseEntity<?> fetchTransactionStatus(
 		@PathVariable Long transactionId
 	) {
