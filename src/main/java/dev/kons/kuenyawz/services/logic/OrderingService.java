3b9e6d1c9490169c57ef8df8a7a10c35c45bc405
narwa <narwandaru.nara@gmail.com>
2024-12-02 13:50:49 +0700

    feat(OrderingService): confirm, findall, and find purchase method
    Took 1 hour 31 minutes

--- src/main/java/dev/kons/kuenyawz/services/logic/OrderingService.java
+++ src/main/java/dev/kons/kuenyawz/services/logic/OrderingService.java
@@ -2,6 +2,9 @@ package dev.kons.kuenyawz.services.logic;
 
 import dev.kons.kuenyawz.dtos.purchase.PurchaseDto;
 import dev.kons.kuenyawz.dtos.purchase.PurchasePostDto;
+import dev.kons.kuenyawz.dtos.purchase.TransactionDto;
+import dev.kons.kuenyawz.services.entity.PurchaseService;
+import org.springframework.data.domain.Page;
 
 public interface OrderingService {
 	/**
@@ -25,4 +28,37 @@ public interface OrderingService {
 	 * @return {@link PurchaseDto}
 	 */
 	PurchaseDto cancelOrder(Long purchaseId);
+
+	/**
+	 * Confirms an order by its purchase id. Confirming an order can only be done if the
+	 * transaction status is already set to "CAPTURE" or "SETTLEMENT.
+	 *
+	 * @param purchaseId {@link Long}
+	 * @return {@link PurchaseDto}
+	 */
+	PurchaseDto confirmOrder(Long purchaseId);
+
+	/**
+	 * Finds all purchases with pagination. Will return based on account session type.
+	 *
+	 * @param criteria {@link PurchaseService.PurchaseSearchCriteria}
+	 * @return {@link Page<PurchaseDto>}
+	 */
+	Page<PurchaseDto> findAll(PurchaseService.PurchaseSearchCriteria criteria);
+
+	/**
+	 * Fetches a purchase by its id.
+	 *
+	 * @param purchaseId {@link Long}
+	 * @return {@link PurchaseDto}
+	 */
+	PurchaseDto findPurchase(Long purchaseId);
+
+	/**
+	 * Fetches the latest transaction of a purchase.
+	 *
+	 * @param purchaseId {@link Long}
+	 * @return {@link TransactionDto}
+	 */
+	TransactionDto findTransactionOfPurchase(Long purchaseId);
 }
