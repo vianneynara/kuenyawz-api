14156a2196728805ff9ba20fcc46cee821fa7662
narwa <narwandaru.nara@gmail.com>
2024-12-04 21:14:15 +0700

    docs(MidtransNotification): set up simple examples
    Took 50 minutes

--- src/main/java/dev/kons/kuenyawz/dtos/midtrans/MidtransNotification.java
+++ src/main/java/dev/kons/kuenyawz/dtos/midtrans/MidtransNotification.java
@@ -4,6 +4,7 @@ package dev.kons.kuenyawz.dtos.midtrans;
 /// [Reference](https://docs.midtrans.com/docs/https-notification-webhooks)
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,46 +16,47 @@ import java.util.List;
  * Notification from Midtrans on payment processes
  * @see <a href="https://docs.midtrans.com/docs/https-notification-webhooks">Midtrans Notification Webhooks Documentation</a>
  */
+@Schema(description = "Notification from Midtrans on payment processes")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class MidtransNotification {
     // Common Fields for All Payment Types
-    @JsonProperty("transaction_time")
+    @JsonProperty("transaction_time") @Schema(example = "2020-01-09 18:27:19")
     private String transactionTime;
 
-    @JsonProperty("transaction_status")
+    @JsonProperty("transaction_status") @Schema(example = "capture")
     private String transactionStatus;
 
-    @JsonProperty("transaction_id")
+    @JsonProperty("transaction_id") @Schema(example = "57d5293c-e65f-4a29-95e4-5959c3fa335b")
     private String transactionId;
 
-    @JsonProperty("status_message")
+    @JsonProperty("status_message") @Schema(example = "midtrans payment notification")
     private String statusMessage;
 
-    @JsonProperty("status_code")
+    @JsonProperty("status_code") @Schema(example = "200")
     private String statusCode;
 
     @JsonProperty("signature_key")
     private String signatureKey;
 
-    @JsonProperty("payment_type")
+    @JsonProperty("payment_type") @Schema(example = "credit_card")
     private String paymentType;
 
     @JsonProperty("order_id")
     private String orderId;
 
-    @JsonProperty("merchant_id")
+    @JsonProperty("merchant_id") @Schema(example = "G123456789")
     private String merchantId;
 
     @JsonProperty("gross_amount")
     private String grossAmount;
 
-    @JsonProperty("fraud_status")
+    @JsonProperty("fraud_status") @Schema(example = "accept")
     private String fraudStatus;
 
-    @JsonProperty("currency")
+    @JsonProperty("currency") @Schema(example = "IDR")
     private String currency;
 
     // Credit Card Specific Fields
@@ -76,7 +78,7 @@ public class MidtransNotification {
     @JsonProperty("bank")
     private String bank;
 
-    @JsonProperty("approval_code")
+    @JsonProperty("approval_code") @Schema(example = "123456")
     private String approvalCode;
 
     // Virtual Account (VA) Specific Fields
@@ -102,10 +104,10 @@ public class MidtransNotification {
     @NoArgsConstructor
     @AllArgsConstructor
     public static class VaNumber {
-        @JsonProperty("va_number")
+        @JsonProperty("va_number") @Schema(example = "1234567890")
         private String vaNumber;
 
-        @JsonProperty("bank")
+        @JsonProperty("bank") @Schema(example = "bca")
         private String bank;
     }
 
@@ -113,7 +115,7 @@ public class MidtransNotification {
     @NoArgsConstructor
     @AllArgsConstructor
     public static class PaymentAmount {
-        @JsonProperty("paid_at")
+        @JsonProperty("paid_at") @Schema(example = "2020-01-09 18:27:19")
         private String paidAt;
 
         @JsonProperty("amount")
