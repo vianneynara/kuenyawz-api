d294bc8052251e28cfc781ba1ea1f9350cd23897
narwa <narwandaru.nara@gmail.com>
2024-12-06 14:36:25 +0700

    test(AccountControllerIT): added get all with accounts test
    Took 14 minutes

--- src/test/java/dev/kons/kuenyawz/controllers/AccountControllerIT.java
+++ src/test/java/dev/kons/kuenyawz/controllers/AccountControllerIT.java
@@ -1,10 +1,10 @@
 package dev.kons.kuenyawz.controllers;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.kons.kuenyawz.boostrappers.DatabaseBootstrapper;
+import dev.kons.kuenyawz.dtos.account.AccountRegistrationDto;
 import dev.kons.kuenyawz.dtos.account.AccountSecureDto;
-import dev.kons.kuenyawz.repositories.AccountRepository;
 import dev.kons.kuenyawz.services.entity.AccountService;
+import jakarta.validation.constraints.NotNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,20 +23,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class AccountControllerIT {
 
 	@Autowired
-	AccountController accountController;
-
-	@Autowired
 	AccountService accountService;
-
-	@Autowired
-	ObjectMapper objectMapper;
 
 	@Autowired
 	WebApplicationContext webApplicationContext;
 
 	MockMvc mockMvc;
-	@Autowired
-	private AccountRepository accountRepository;
 
 	@BeforeEach
 	void setUp() {
@@ -44,10 +36,30 @@ public class AccountControllerIT {
 	}
 
 	@Test
-	void testGetAllAccounts() {
+	void testGetAllAccounts_DefaultEmpty() {
 		List<AccountSecureDto> accounts = accountService.getAllAccounts();
 
-		assertThat(accounts).isNotEmpty();
+		assertThat(accounts).isEmpty();
 	}
 
+	@Test
+	void testGetAllAccounts_WithAccounts() {
+		// Arrange
+		insertTestAccount("012345678901", "Test Account 1");
+		insertTestAccount("012345678902", "Test Account 2");
+
+		List<AccountSecureDto> accounts = accountService.getAllAccounts();
+
+		assertThat(accounts).hasSize(2);
+	}
+
+	void insertTestAccount(@NotNull String phone, @NotNull String fullName) {
+		accountService.createAccount(
+			AccountRegistrationDto.builder()
+				.phone(phone)
+				.fullName(fullName)
+				.password("password")
+				.build()
+		);
+	}
 }
