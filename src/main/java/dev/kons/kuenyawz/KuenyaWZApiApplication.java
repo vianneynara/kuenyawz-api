7a4efb69cca82fb3cbf4d64083b2344f6b9111a3
narwa <narwandaru.nara@gmail.com>
2024-12-08 18:35:57 +0700

    feat(KuenyaWZApiApplication): enable caching
    Took 36 seconds

--- src/main/java/dev/kons/kuenyawz/KuenyaWZApiApplication.java
+++ src/main/java/dev/kons/kuenyawz/KuenyaWZApiApplication.java
@@ -3,11 +3,13 @@ package dev.kons.kuenyawz;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableConfigurationProperties
+@EnableCaching
 public class KuenyaWZApiApplication {
 
 	public static void main(String[] args) {
